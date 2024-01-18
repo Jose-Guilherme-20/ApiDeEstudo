e281896e8afb25f3ab0044238da5be665adea400
José-Guilherme-20 <jgscosta20@gmail.com>
2024-01-17 22:39:07 -0300

    chore: atualização net 6.0

--- Crud-Api/Program.cs
+++ Crud-Api/Program.cs
@@ -1,26 +1,73 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using Crud_Api.Context;
+using Crud_Api.Repository;
+using Crud_Api.Repository.Interfaces;
+using Crud_Api.Services;
+using Crud_Api.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
+using Repository;
+using Repository.Interfaces;
+using Services;
+using Services.Interfaces;
 
-namespace Crud_Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped(typeof(IEntityBaseRepository<>), typeof(EntityBaseRepository<>));
+
+var connection = builder.Configuration["ConnectionStrings:Connection"];
+builder.Services.AddDbContext<EntityContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(5, 0, 0))));
+builder.Services.AddSingleton<DbConnection>(conn => new MySqlConnection(connection));
+builder.Services.AddScoped<DapperContext>();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Nome do seu API", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nome do seu API v1");
+    // A linha abaixo serve para acessar a documentação Swagger na raiz do seu aplicativo.
+    // Certifique-se de que isso não entra em conflito com outras rotas em seu aplicativo.
+    c.RoutePrefix = string.Empty;
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- Crud-Api/Startup.cs
+++ Crud-Api/Startup.cs
@@ -1,168 +1,161 @@
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Text;
-using Crud_Api.Context;
-using Crud_Api.Repository;
-using Crud_Api.Repository.Interfaces;
-using Crud_Api.Services;
-using Crud_Api.Services.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Rewrite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
-using Microsoft.OpenApi.Models;
-using MySqlConnector;
-using Repository;
-using Repository.Interfaces;
-using Services;
-using Services.Interfaces;
+// using System;
+// using System.Collections.Generic;
+// using System.Data.Common;
+// using System.Text;
+// using Crud_Api.Context;
+// using Crud_Api.Repository;
+// using Crud_Api.Repository.Interfaces;
+// using Crud_Api.Services;
+// using Crud_Api.Services.Interfaces;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Rewrite;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.DependencyInjection.Extensions;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.IdentityModel.Tokens;
+// using Microsoft.Net.Http.Headers;
+// using Microsoft.OpenApi.Models;
+// using MySqlConnector;
+// using Repository;
+// using Repository.Interfaces;
+// using Services;
+// using Services.Interfaces;
 
-namespace Crud_Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+// namespace Crud_Api
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration, IWebHostEnvironment env)
+//         {
+//             Configuration = configuration;
+//         }
 
-        public IConfiguration Configuration { get; }
+//         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
 
-            var key = Encoding.ASCII.GetBytes(Configuration["Key"]);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer("Bearer", x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-            services.AddAuthorization(auth =>
-            {
-                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-                .RequireAuthenticatedUser().Build());
-            });
+//             var key = Encoding.ASCII.GetBytes(Configuration["Key"]);
+//             services.AddAuthentication(x =>
+//             {
+//                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//             })
+//             .AddJwtBearer("Bearer", x =>
+//             {
+//                 x.RequireHttpsMetadata = false;
+//                 x.SaveToken = true;
+//                 x.TokenValidationParameters = new TokenValidationParameters
+//                 {
+//                     ValidateIssuerSigningKey = true,
+//                     IssuerSigningKey = new SymmetricSecurityKey(key),
+//                     ValidateIssuer = false,
+//                     ValidateAudience = false
+//                 };
+//             });
+//             services.AddAuthorization(auth =>
+//             {
+//                 auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+//                 .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+//                 .RequireAuthenticatedUser().Build());
+//             });
 
-            services.AddCors(options => options.AddDefaultPolicy(builder =>
-            {
-                builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-            }));
-            services.AddControllers();
-            services.AddMvc(options =>
-            {
-                options.RespectBrowserAcceptHeader = true;
-                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
-                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
-            })
-            .AddXmlSerializerFormatters();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sua API", Version = "v1" });
+//             services.AddCors(options => options.AddDefaultPolicy(builder =>
+//             {
+//                 builder.AllowAnyOrigin()
+//                 .AllowAnyMethod()
+//                 .AllowAnyHeader();
+//             }));
+//             services.AddControllers();
+//             services.AddMvc(options =>
+//             {
+//                 options.RespectBrowserAcceptHeader = true;
+//                 options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+//                 options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+//             })
+//             .AddXmlSerializerFormatters();
+//             services.AddSwaggerGen(c =>
+//             {
+//                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sua API", Version = "v1" });
 
-                // Configuração para incluir a autenticação JWT no Swagger
-                var securityScheme = new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                };
+//                 // Configuração para incluir a autenticação JWT no Swagger
+//                 var securityScheme = new OpenApiSecurityScheme
+//                 {
+//                     Reference = new OpenApiReference
+//                     {
+//                         Type = ReferenceType.SecurityScheme,
+//                         Id = "Bearer"
+//                     }
+//                 };
 
-                var securityRequirements = new OpenApiSecurityRequirement
-                {
-                        { securityScheme, new List<string>() }
-                };
+//                 var securityRequirements = new OpenApiSecurityRequirement
+//                 {
+//                         { securityScheme, new List<string>() }
+//                 };
 
-                c.AddSecurityDefinition("Bearer", securityScheme);
-                c.AddSecurityRequirement(securityRequirements);
-            });
-            // Dependecy Injection 
-            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddScoped<IPersonRepository, PersonRepository>();
-            services.AddScoped<IBookRepository, BookRepository>();
-            services.AddScoped<IPersonService, PersonService>();
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IBookService, BookService>();
-            services.AddScoped<IFileService, FileService>();
-            services.AddScoped(typeof(IEntityBaseRepository<>), typeof(EntityBaseRepository<>));
+//                 c.AddSecurityDefinition("Bearer", securityScheme);
+//                 c.AddSecurityRequirement(securityRequirements);
+//             });
+//             // Dependecy Injection 
+//             services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//             services.AddScoped<IPersonRepository, PersonRepository>();
+//             services.AddScoped<IBookRepository, BookRepository>();
+//             services.AddScoped<IPersonService, PersonService>();
+//             services.AddScoped<ITokenService, TokenService>();
+//             services.AddScoped<IBookService, BookService>();
+//             services.AddScoped<IFileService, FileService>();
+//             services.AddScoped(typeof(IEntityBaseRepository<>), typeof(EntityBaseRepository<>));
 
+//             RegisterDatabaseServices(services);
 
+//         }
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             app.UseHttpsRedirection();
+//             app.UseRouting();
+//             app.UseCors();
 
-
-            RegisterDatabaseServices(services);
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
+//             app.UseSwagger();
+//             app.UseSwaggerUI();
 
 
 
-            app.UseCors(x =>
-            x.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+//             app.UseCors(x =>
+//             x.AllowAnyOrigin()
+//             .AllowAnyMethod()
+//             .AllowAnyHeader());
 
-            app.UseSwagger();
-            app.UseSwaggerUI();
+//             app.UseAuthentication();
 
-            var option = new RewriteOptions();
-            option.AddRedirect("^$", "swagger");
-            app.UseRewriter(option);
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapControllerRoute("DefaultApi", "{controller=values}/{id?}");
-            });
 
-        }
-        protected virtual void RegisterDatabaseServices(IServiceCollection services)
-        {
-            var connection = Configuration["ConnectionStrings:Connection"];
-            services.AddDbContext<EntityContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(5, 0, 0))));
-            services.AddSingleton<DbConnection>(conn => new MySqlConnection(connection));
-            services.AddScoped<DapperContext>();
-        }
-    }
-}
+//             var option = new RewriteOptions();
+//             option.AddRedirect("^$", "swagger");
+//             app.UseRewriter(option);
+//             app.UseAuthorization();
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllers();
+//                 endpoints.MapControllerRoute("DefaultApi", "{controller=Values}/{id?}");
+//             });
+
+//         }
+//         protected virtual void RegisterDatabaseServices(IServiceCollection services)
+//         {
+//             var connection = Configuration["ConnectionStrings:Connection"];
+//             services.AddDbContext<EntityContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(5, 0, 0))));
+//             services.AddSingleton<DbConnection>(conn => new MySqlConnection(connection));
+//             services.AddScoped<DapperContext>();
+//         }
+//     }
+// }
 
